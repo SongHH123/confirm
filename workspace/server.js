94a44931b4064608c1645fe085b7ca8e23aff9d0
SongHyeHyeon <1971318@hansung.ac.kr>
2021-07-30 22:02:18 +0900

    fix error

--- workspace/server.js
+++ workspace/server.js
@@ -1,31 +1,11 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
+const express = require('express')
+const app = express()
+const port = 4000
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
-var server = http.createServer(function(request,response){
-  var parsedUrl = url.parse(request.url);
-  var resource = parsedUrl.pathname;
+app.use(express.static('html'));
 
-  // 1. 요청된 자원이 /hello 이면
-  if(resource == '/login'){
-    // 2. hello.html 파일을 읽은 후
-    fs.readFile('./html/login.html', 'utf-8', function(error, data) {
-      // 2.1 읽으면서 오류가 발생하면 오류의 내용을
-      if(error){
-        response.writeHead(500, {'Content-Type':'text/html'});
-        response.end('500 Internal Server Error : '+error);
-      // 2.2 아무런 오류가 없이 정상적으로 읽기가 완료되면 파일의 내용을 클라이언트에 전달
-      }else{
-        response.writeHead(200, {'Content-Type':'text/html'});
-        response.end(data);
-      }
-    });
-  }else{
-    response.writeHead(404, {'Content-Type':'text/html'});
-    response.end('404 Page Not Found');
-  }
-});
-
-server.listen(80, function(){
-    console.log('Server is running...');
-});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}/login.html`)
+})
