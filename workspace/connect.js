1d805e77019b032068a43fb07eaa77a0528d469e
SongHyeHyeon <1971318@hansung.ac.kr>
2021-08-17 10:15:18 +0900

    fix function

--- workspace/connect.js
+++ workspace/connect.js
@@ -1,3 +1,14 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  password:'1234',
+  database: 'school',
+  port: '3306',
+});
+
+connection.connect();
+//서버 실행시 삭제
 export function Confirm_St(uname, psw){
   connection.query('SELECT stu_id, user_pw from student_info', (error, results, fields) => {
     if(error) throw error;
@@ -27,5 +38,6 @@ export function Confirm_Tr(uname, psw){
     return -1;
   });
 }
+
 
 //connection.end();
--- workspace/login_process.js
+++ workspace/login_process.js
@@ -3,15 +3,15 @@ import {Confirm_St, Confirm_Tr} from "./connect.js";
 export function goPage(role, uname, psw){
   if(role == "st"){
     if(Confirm_St(uname, psw)){
-      return Confirm_St(uname, psw);
+      return "st_main.html";
     }else {
       return -1;
     }
   }else {
     if(Confirm_Tr(uname, psw)){
-      return 10000 + Confirm_Tr(uname, psw);
+      return "tr_main.html";
     }else {
-      return -1;
+      return -2;
     }
   }
 }
